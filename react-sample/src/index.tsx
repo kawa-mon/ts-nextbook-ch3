8376156ea66f188418fe1810b028dfca0dd39ecf
川島慎悟 <kawashingo1990@gmail.com>
2022-10-23 06:16:01 +0900

    htmlの属性の中には予約語と被るなどしてそのまま使えない宣言がある

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 // import App from './App';
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
+import Name from './components/Name'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Hello />
+    {/* <Hello /> */}
+    <Name />
   </React.StrictMode>
 )
 
