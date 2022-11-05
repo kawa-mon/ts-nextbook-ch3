bc30e68ada7d28a4671f8297b95099a45f9bcca8
川島慎悟 <kawashingo1990@gmail.com>
2022-11-06 05:53:52 +0900

    コンポーネントの例（propsで親から子にデータを一方通行で渡せる、フックの登場で関数コンポーネントが主流になった）

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 // import App from './App';
 // import Hello from './components/Hello'
-import Name from './components/Name'
+// import Name from './components/Name'
+// import Message from './components/Message'
+import Parent from './components/ContainerSample'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
@@ -11,7 +13,9 @@ root.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Hello /> */}
-    <Name />
+    {/* <Name /> */}
+    {/* <Message /> */}
+    <Parent />
   </React.StrictMode>
 )
 
